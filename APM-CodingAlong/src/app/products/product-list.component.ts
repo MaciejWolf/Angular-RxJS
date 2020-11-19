13336be7ba00302ac4e43470109a83fd9e29d200
MaciejWolf <m_wolf@interia.pl>
2020-11-20 00:09:34 +0100

    Declarative pattern for data retrieval

--- APM-CodingAlong/src/app/products/product-list.component.ts
+++ APM-CodingAlong/src/app/products/product-list.component.ts
@@ -1,8 +1,7 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { EMPTY, Observable } from 'rxjs';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { EMPTY } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-import { Product } from './product';
 import { ProductService } from './product.service';
 
 @Component({
@@ -10,24 +9,20 @@ import { ProductService } from './product.service';
   styleUrls: ['./product-list.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   categories;
 
-  products$: Observable<Product[]>;
+  products$ = this.productService.products$
+    .pipe(
+      catchError(err => {
+        this.errorMessage = err
+        return EMPTY
+      })
+    )
 
   constructor(private productService: ProductService) { }
-
-  ngOnInit(): void {
-    this.products$ = this.productService.getProducts()
-      .pipe(
-        catchError(err => {
-          this.errorMessage = err
-          return EMPTY
-        })
-      )
-  }
 
   onAdd(): void {
     console.log('Not yet implemented');
