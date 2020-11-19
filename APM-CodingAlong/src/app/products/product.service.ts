648fa6d771d69111517a3781d0854f08684d62a3
MaciejWolf <m_wolf@interia.pl>
2020-11-20 00:15:28 +0100

    Declarative pattern for product-list-alt

--- APM-CodingAlong/src/app/products/product.service.ts
+++ APM-CodingAlong/src/app/products/product.service.ts
@@ -24,14 +24,6 @@ export class ProductService {
   constructor(private http: HttpClient,
               private supplierService: SupplierService) { }
 
-  getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.productsUrl)
-      .pipe(
-        tap(data => console.log('Products: ', JSON.stringify(data))),
-        catchError(this.handleError)
-      );
-  }
-
   private fakeProduct(): Product {
     return {
       id: 42,
