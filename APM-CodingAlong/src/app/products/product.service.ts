c8488bb4deb6aafc6a49a439bb7c7e5d1a9f12d8
MaciejWolf <m_wolf@interia.pl>
2020-11-21 00:30:15 +0100

    add caching

--- APM-CodingAlong/src/app/products/product.service.ts
+++ APM-CodingAlong/src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { BehaviorSubject, combineLatest, merge, Observable, Subject, throwError } from 'rxjs';
-import { catchError, map, scan, tap } from 'rxjs/operators';
+import { catchError, map, scan, shareReplay, tap } from 'rxjs/operators';
 
 import { Product } from './product';
 import { Supplier } from '../suppliers/supplier';
@@ -37,7 +37,8 @@ export class ProductService {
           price: product.price * 1.5,
           categoryName: categories.find(c => c.id === product.categoryId).name,
           searchKey: [product.productName]
-        }) as Product)));
+        }) as Product)),
+      shareReplay(1));
 
   private productSelectedSubject = new BehaviorSubject<number>(0);
   productSelectedAction$ = this.productSelectedSubject.asObservable();
@@ -49,7 +50,8 @@ export class ProductService {
       map(([products, selectedProductId]) => 
         products.find(product => product.id === selectedProductId)
       ),
-      tap(product => console.log('selectedProduct', product))
+      tap(product => console.log('selectedProduct', product)),
+      shareReplay(1)
     );
 
   private productInsertedSubject = new Subject<Product>();
